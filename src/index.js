e27d870a6f26b34faddd5d0712d78dd33291c1ee
uroma <uroma@accessbankplc.com>
2022-06-13 14:12:06 +0100

    set up project and install necessary packages

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import routes from "./api/routes/route";
+import cors from "cors";
+
+dotenv.config();
+const mongoString = process.env.DATABASE_URL;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+const app = express();
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api", routes);
+
+app.listen(8000, () => {
+  console.log(`Server Started at ${8000}`);
+});
